309ff25a37278001c2dcccd925e0259d817d1fef
JakobGrosen <sunfolk88@gmail.com>
2016-06-20 14:51:23 +0200

    user + rights classes

---
+++ include/class/rights.class.php
@@ -0,0 +1,9 @@
+<?php
+    class Rights
+    {
+        public $id;
+        public $prefixCategory;
+        public $prefix;
+        public $title;
+    }
+?>
---
+++ include/class/user.class.php
@@ -0,0 +1,12 @@
+<?php
+    class User
+    {
+        public $id;
+        public $languageId;
+        public $userTypeId;
+
+        public $firstname;
+        public $surname;
+        public $email;
+    }
+?>

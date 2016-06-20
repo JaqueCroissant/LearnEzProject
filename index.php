41d9a86b4968a8e971f374a5cb6d704e6fe8937b
mathiaskarl <mathiasklinke@gmail.com>
2016-06-20 14:06:33 +0200

    NetBeans project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>LearnEZ</title>
+    </head>
+    <body>
+        <?php
+            echo "hello world";
+        ?>
+    </body>
+</html>

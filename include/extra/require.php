378f61a9a87c348aed846fd01db16220c2e2d01f
Krazh <moped47@gmail.com>
2016-06-21 11:50:01 +0200

    implemented errorHandler into basic design

---
+++ include/extra/require.php
@@ -0,0 +1,3 @@
+<?php
+    require_once 'include/handler/errorHandler.php';
+?>
--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
+<?php
+require_once 'include/extra/require.php';
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>LearnEZ</title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php
-            echo "hello world";
-        ?>
+        <div class="col-xs-12 navbar-default pull-right">
+            
+            <?php
+            include 'include/topbar.php';
+            ?>
+
+        </div>
+        <div class="col-xs-1 navbar-default">
+            <ul>
+                <li>first</li>
+                <li>second</li>
+                <li>third</li>
+
+            </ul>
+
+        </div>
     </body>
 </html>

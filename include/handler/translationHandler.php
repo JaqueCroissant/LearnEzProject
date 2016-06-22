a3a55f9cbd6891bca58630ff9e437e34071fada8
mrhallberg <mr.hallberg@hotmail.com>
2016-06-22 16:58:26 +0200

    Added translationHandler

--- include/handler/translationHandler.php
+++ include/handler/translationHandler.php
@@ -2,17 +2,24 @@
 class TranslationHandler
 {
     private $_defaultLanguage = 1;
+    private $_currentLanguage;
     
-    public $primaryTranslations;
+    public $translation_static_text;
     
     public function __construct(){
-        $this->primaryTranslations = $this->getPrimary();
+    }
+    
+    public function loadStaticTexts(){
+        $this->translation_static_text = $this->getStaticText();
     }
     
     public function getLanguage(){
-        $slang = SessionKeyHandler::GetFromSession("user")->languageId;
-        if (!empty($slang)) {
-            return $slang;
+        if (isset($this->_currentLanguage)) {
+            return $this->_currentLanguage;
+        }
+        $user = SessionKeyHandler::GetFromSession("user");
+        if (!empty($user)) {
+            return $user->languageId;
         }
         $clang = $_COOKIE["language_id"];
         if (!empty($clang)) {
@@ -23,23 +30,36 @@ class TranslationHandler
     
     public function setLanguage($language){
         setcookie("language_id", $language);
+        $this->_currentLanguage = $language;
         if (SessionKeyHandler::SessionExists("user")) {
             $user = SessionKeyHandler::GetFromSession("user");
             $user->languageId = $language;
             SessionKeyHandler::AddToSession("user", $user);
-            //TODO Update db
+            DbHandler::getInstance()->Query("UPDATE users SET language_id=:languageId WHERE id=:userId", $language, $user->id);
         }
+        $this->updateStaticText();
     }
     
-    private function getPrimary(){
-        $trans = SessionKeyHandler::GetFromSession("primaryTranslations");
+    private function getStaticText(){
+        $trans = SessionKeyHandler::GetFromSession("static_text");
         if (!empty($trans)) {
             return $trans;
         }
-        //TODO get data from db
-        $dbdata = [];
-        SessionKeyHandler::AddToSession("primaryTranslations", $dbdata);
-        return $dbdata;       
+        $this->updateStaticText();
+        return SessionKeyHandler::GetFromSession("static_text");
+    }
+    
+    private function updateStaticText(){
+        $dbdata = DbHandler::getInstance()->ReturnQuery("SELECT static_text.prefix, translation_static_text.text "
+                . "FROM translation_static_text "
+                . "INNER JOIN static_text "
+                . "ON static_text.id = translation_static_text.static_text_id "
+                . "WHERE language_id=:languageId", $this->getLanguage());
+        $finalArray = array();
+        foreach ($dbdata as $value) {
+            $finalArray[$value["prefix"]] = $value["text"];
+        }
+        SessionKeyHandler::AddToSession("static_text", $finalArray);
     }
 }
 ?>

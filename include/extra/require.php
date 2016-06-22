de3cbb6eb5f50af936bf5fe7242e5d7fcdd749b6
JakobGrosen <sunfolk88@gmail.com>
2016-06-22 13:58:37 +0200

    opdateret require + required datamodels

--- include/extra/require.php
+++ include/extra/require.php
@@ -1,12 +1,13 @@
 <?php
-    require_once "include/extra/db.class.php";
+    require_once 'include/extra/required_datamodels.php';
     require_once 'include/handler/errorHandler.php';
     require_once 'include/handler/dbHandler.php';
     require_once 'include/handler/sessionKeyHandler.php';
+    require_once 'include/handler/rightsHandler.php';
     require_once 'include/pages/pagelist.php';
     
-    
     $dbHandler = new DbHandler($db_username, $db_password);
+    $rightsHandler = new RightsHandler();
 
 
     

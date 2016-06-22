944486e5a39e196e299b659ef0f5c603d0e9b260
mathiaskarl <mathiasklinke@gmail.com>
2016-06-22 14:09:06 +0200

    corrected errors from merge.

--- include/extra/require.php
+++ include/extra/require.php
@@ -1,33 +1,13 @@
 <?php
-<<<<<<< HEAD
+    session_start();
     require_once 'include/extra/required_datamodels.php';
     require_once 'include/handler/errorHandler.php';
     require_once 'include/handler/dbHandler.php';
     require_once 'include/handler/sessionKeyHandler.php';
     require_once 'include/handler/rightsHandler.php';
-=======
-    session_start();
-    require_once "include/extra/db.class.php";
-    require_once "include/class/error.class.php";
-    require_once 'include/handler/errorHandler.php';
-    require_once 'include/handler/dbHandler.php';
-    require_once 'include/handler/sessionKeyHandler.php';
     require_once 'include/handler/loginHandler.php';
->>>>>>> 3fbe7e7565457bd1fc8277c794fac4b255e4b202
     require_once 'include/pages/pagelist.php';
-    
-<<<<<<< HEAD
-    
+
     $loginHandler = new LoginHandler();
-=======
-    $dbHandler = new DbHandler($db_username, $db_password);
-<<<<<<< HEAD
     $rightsHandler = new RightsHandler();
-
-
-    
-=======
-    $loginHandler = new LoginHandler($dbHandler);
->>>>>>> 3fbe7e7565457bd1fc8277c794fac4b255e4b202
->>>>>>> bdaa06286f85a2b8c357368d8393e71976c2bbd6
 ?>
--- include/extra/required_datamodels.php
+++ include/extra/required_datamodels.php
@@ -2,4 +2,5 @@
     require_once 'include/extra/db.class.php';
     require_once 'include/class/user.class.php';
     require_once 'include/class/rights.class.php';
+    require_once 'include/class/error.class.php';
 ?>

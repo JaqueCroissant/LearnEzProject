b7eb2f041c6f8eb0ee8f1c53be908aa6fe01754b
mrhallberg <mr.hallberg@hotmail.com>
2016-06-23 14:21:16 +0200

    Creating notificationHandler and notification.class

---
+++ include/class/notification.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class Notification extends ORM
+    {
+        public $id;
+        public $title;
+        public $text;
+        public $datetime;
+        public $isRead;
+        public $sender_name;
+    }
+

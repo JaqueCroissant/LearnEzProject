53438d1bd34c436ba03de351fbe60b1fd4b7bf56
mathiaskarl <mathiasklinke@gmail.com>
2016-06-23 12:49:54 +0200

    ORM opdateret

--- include/class/user.class.php
+++ include/class/user.class.php
@@ -1,5 +1,5 @@
 <?php
-    class User
+    class User extends ORM
     {
         public $id;
         public $language_id;
@@ -10,26 +10,5 @@
         public $firstname;
         public $surname;
         public $email;
-        
-        private $data_array;
-        
-        public function __construct($user_info) {
-            if(!is_array($user_info)) {
-                return;
-            }
-
-            $this->data_array = $user_info;
-            $this->iterate_properties();
-            $this->data_array = null;
-        }
-        
-        private function iterate_properties() {
-            foreach(get_object_vars($this) as $key => $value) {
-                if(array_key_exists($key, $this->data_array)) {
-                    $this->$key = $this->data_array[$key];
-                }
-            }
-        }
-        
     }
 ?>
--- include/extra/required_datamodels.php
+++ include/extra/required_datamodels.php
@@ -1,5 +1,6 @@
 <?php
     require_once 'include/extra/db.class.php';
+    require_once 'include/class/orm.class.php';
     require_once 'include/class/user.class.php';
     require_once 'include/class/rights.class.php';
     require_once 'include/class/error.class.php';

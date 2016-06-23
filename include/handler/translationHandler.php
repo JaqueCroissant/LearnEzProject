9c0635ec421b929876796dce5dcc4ccf4d5482b4
mrhallberg <mr.hallberg@hotmail.com>
2016-06-23 11:23:15 +0200

    Fixed TranslationHandler

--- include/handler/translationHandler.php
+++ include/handler/translationHandler.php
@@ -1,60 +1,70 @@
 <?php
 class TranslationHandler
 {
-    private $_defaultLanguage = 1;
-    private $_currentLanguage;
-    
-    public $translation_static_text;
+    private $_defaultLanguage = 1; 
+    private$translation_static_text;
     
     public function __construct(){
+        if (!SessionKeyHandler::SessionExists("current_language")) {
+            $this->setCurrentLanguage($this->loadLanguageSettings());
+        }
+        $this->loadStaticTexts();
     }
     
     public function loadStaticTexts(){
-        $this->translation_static_text = $this->getStaticText();
+        $trans = SessionKeyHandler::GetFromSession("static_text");
+        if (!empty($trans)) {
+            $this->translation_static_text = $trans;
+        }
+        else {
+            $this->updateStaticText();
+            $this->translation_static_text = SessionKeyHandler::GetFromSession("static_text");
+        }
     }
     
-    public function getLanguage(){
-        if (isset($this->_currentLanguage)) {
-            return $this->_currentLanguage;
+    public static function getCurrentLanguage(){
+        return SessionKeyHandler::GetFromSession("current_language");
+    }
+    
+    public function getStaticText($key){
+        return $this->translation_static_text[$key];
+    }
+    
+    public function setLanguage($language){
+        if ($language != TranslationHandler::getCurrentLanguage()) {
+            setcookie("language_id", $language);
+            if (SessionKeyHandler::SessionExists("user")) {
+                $user = SessionKeyHandler::GetFromSession("user");
+                $user->languageId = $language;
+                SessionKeyHandler::AddToSession("user", $user);
+                DbHandler::getInstance()->Query("UPDATE users SET language_id=:languageId WHERE id=:userId", $language, $user->id);
+            }
+            TranslationHandler::setCurrentLanguage($language);
+            TranslationHandler::updateStaticText();
+            $this->loadStaticTexts();
         }
-        $user = SessionKeyHandler::GetFromSession("user");
-        if (!empty($user)) {
-            return $user->languageId;
+    }
+    
+    private static function setCurrentLanguage($language){
+        SessionKeyHandler::AddToSession("current_language", $language);
+    }
+    
+    private function loadLanguageSettings(){
+        if (SessionKeyHandler::SessionExists("user")){
+            return SessionKeyHandler::GetFromSession("user")->languageId;
         }
-        $clang = $_COOKIE["language_id"];
-        if (!empty($clang)) {
-            return $clang;
+        if (isset($_COOKIE["language_id"])) {
+            return $_COOKIE["language_id"];            
         }
         return $this->_defaultLanguage;
     }
     
-    public function setLanguage($language){
-        setcookie("language_id", $language);
-        $this->_currentLanguage = $language;
-        if (SessionKeyHandler::SessionExists("user")) {
-            $user = SessionKeyHandler::GetFromSession("user");
-            $user->languageId = $language;
-            SessionKeyHandler::AddToSession("user", $user);
-            DbHandler::getInstance()->Query("UPDATE users SET language_id=:languageId WHERE id=:userId", $language, $user->id);
-        }
-        $this->updateStaticText();
-    }
-    
-    private function getStaticText(){
-        $trans = SessionKeyHandler::GetFromSession("static_text");
-        if (!empty($trans)) {
-            return $trans;
-        }
-        $this->updateStaticText();
-        return SessionKeyHandler::GetFromSession("static_text");
-    }
-    
-    private function updateStaticText(){
+    private static function updateStaticText(){
         $dbdata = DbHandler::getInstance()->ReturnQuery("SELECT static_text.prefix, translation_static_text.text "
                 . "FROM translation_static_text "
                 . "INNER JOIN static_text "
                 . "ON static_text.id = translation_static_text.static_text_id "
-                . "WHERE language_id=:languageId", $this->getLanguage());
+                . "WHERE language_id=:languageId", TranslationHandler::getCurrentLanguage());
         $finalArray = array();
         foreach ($dbdata as $value) {
             $finalArray[$value["prefix"]] = $value["text"];
--- test.php
+++ test.php
@@ -3,5 +3,23 @@
     //$dbHandler->Query("UPDATE `rights` SET prefix = :prefix, sort_order = :sort_order WHERE id = :id", "JAKOB", 10, 1);
     //echo DbHandler::getInstance()->CountQuery("SELECT * FROM `rights` WHERE prefix = :prefix AND sort_order = :sort_order", "HEJ", 5);
     //require_once 'include/handler/sessionKeyHandler.php';
-
+//    echo "Current language: " . TranslationHandler::getCurrentLanguage();
+//    $trans = new TranslationHandler();
+//    echo "<br/>";
+//    echo "Current language: " . $trans->getCurrentLanguage(); 
+//    echo "<br/>";
+//    echo "Danish prefix: " . $trans->getStaticText("DANISH");
+//    $trans->setLanguage(2);
+//    echo "<br/>";
+//    echo "Current language: " . $trans->getCurrentLanguage();
+//    echo "<br/>";
+//    echo "Danish prefix: " . $trans->getStaticText("DANISH");
+//    $trans->setLanguage(1);
+//    echo "<br/>";
+//    echo "Current language: " . $trans->getCurrentLanguage();
+//    echo "<br/>";
+//    echo "Danish prefix: " . $trans->getStaticText("ENGLISH");
+    
+    
+?>
 

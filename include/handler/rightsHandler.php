e8adb38997daeb86ee5bb92bb1f6c8e6fc818c69
JakobGrosen <sunfolk88@gmail.com>
2016-06-23 11:35:28 +0200

    Tilføjet "UpdateTypeRights" til RightsHandler.php

--- include/handler/rightsHandler.php
+++ include/handler/rightsHandler.php
@@ -31,6 +31,42 @@
             return false;
         }
 
+        public function UpdateTypeRights($user_type, $rights_array)
+        {
+            if(is_int($user_type) && $user_type < 5 && $user_type > 0)
+            {
+                DbHandler::getInstance()->Query("DELETE FROM user_type_rights WHERE user_type_id = :id", $user_type);
+                $rights = DbHandler::getInstance()->ReturnQuery("SELECT * FROM rights");
+                
+                $new_rights = array();
+                foreach($rights as $right)
+                {
+                    if(in_array($right['prefix'], $rights_array))
+                    {
+                        array_push($new_rights, $right);
+                    }
+                }
 
+                $right_count = count($new_rights);
+                $insert_values = "";
+                
+                for($i=0; $i<$right_count; $i++)
+                {
+                    if($i != 0 && $i!=$right_count)
+                    {
+                        $insert_values .= ", ";
+                    }
+
+                    $right = $new_rights[$i];
+                    $insert_values .= "(" . $user_type . ", " . $right['id'] . ")";
+                }
+
+                DbHandler::getInstance()->Query("INSERT INTO user_type_rights (user_type_id, rights_id) VALUES " . $insert_values);
+
+                return true;
+            }
+
+            return false;
+        }
     }
 ?>

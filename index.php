0818e66dae7d267cad60dc68acd09edfc83414bf
mrhallberg <mr.hallberg@hotmail.com>
2016-06-23 15:32:47 +0200

    mathias sagde jeg skulle pushe

--- index.php
+++ index.php
@@ -42,6 +42,7 @@ require_once 'include/extra/require.php';
             } else {
                 include('include/pages/front.php');
             }
+            
         ?>
         </div>
         <div id="notificationWindow">

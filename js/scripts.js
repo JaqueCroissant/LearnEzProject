3fb6987f048998da3e5cc99e5a5fc4191b66a823
Krazh <moped47@gmail.com>
2016-06-23 14:18:47 +0200

    Design ændringer - ved at være klar til første fremvisning. Ændringer inkluderer box shadows, hover effekter, fixes af sidebar bugs samt logo+title.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,44 @@
 jQuery(function ($) {
     $(document).ready(function () {
         $('#notificationWindow').hide();
+        $('#navBar').addClass("collapsed");
+        $('#navBar .menu_text').hide();
+        
+        function HideSidebarOnTablet () {
+            $('.sidebar').addClass("hidden-sm hidden-xs collapsed");
+        }
+        
         $("html").click(function () {
             $("#notificationWindow").hide("fast");
+            HideSidebarOnTablet();
         });
+        
+        // Sidebar expand funktion
+        var navBarTransitionDur = $('#navBar').css("transition-duration");
+            navBarTransitionDur = 1000 * parseFloat(navBarTransitionDur);
+
+        var menuTextTimeout;
+        function showMenuText() {
+            menuTextTimeout = setTimeout(function () {
+                $('#navBar .menu_text').show();
+            }, navBarTransitionDur);
+        }
+        
         $('#navBar').hover(function () {
-            $('#navBar').removeClass("collapsed");
-            $('#navBar').addClass("expanded");
-            $('.menu_text').switchClass("collapsedTitle", "visible");
+            $('#navBar').switchClass("collapsed", "expanded");
+            showMenuText();
         }, function () {
-            $('#navBar .menu_text').removeClass("visible");
-            $('#navBar .menu_text').addClass("collapsedTitle");
-            $('#navBar').removeClass("expanded");
-            $('#navBar').addClass("collapsed");
-        }); 
+            clearTimeout(menuTextTimeout);
+            $('#navBar .menu_text').hide();
+            $('#navBar').switchClass("expanded", "collapsed");
+            HideSidebarOnTablet();
+        });
 
         var top = $('.topbar').height();
+        var left = $('.collapsed').width();
         $('.sidebar').css("top", top);
-        $('.content').css("padding-top", top);
+        $('.content').css("padding-top", top + 10);
+        $('.content').css("padding-left", left + 10);
         $('#notificationWindow').css("top", top + 5);
         $('#notificationWindow').css("right", 40);
 
@@ -37,6 +58,11 @@ jQuery(function ($) {
             } else {
                 $('#notificationWindow').hide("fast");
             }
+        });
+        
+        $('#sidebarButton').click(function (e) {
+            e.stopPropagation();
+            $('.sidebar').removeClass("hidden-sm hidden-xs collapsed");
         });
     });
  });

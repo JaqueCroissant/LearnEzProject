ec5ca09efbd3bb5de63b1396aacff9e3b0c42e95
mathiaskarl <mathiasklinke@gmail.com>
2016-06-23 12:39:59 +0200

    test conflict fixed

--- test.php
+++ test.php
@@ -3,9 +3,7 @@
     //$dbHandler->Query("UPDATE `rights` SET prefix = :prefix, sort_order = :sort_order WHERE id = :id", "JAKOB", 10, 1);
     //echo DbHandler::getInstance()->CountQuery("SELECT * FROM `rights` WHERE prefix = :prefix AND sort_order = :sort_order", "HEJ", 5);
     //require_once 'include/handler/sessionKeyHandler.php';
-<<<<<<< HEAD
-    $user = new User(array());
-=======
+    //$user = new User(array());
 //    echo "Current language: " . TranslationHandler::getCurrentLanguage();
 //    $trans = new TranslationHandler();
 //    echo "<br/>";
@@ -22,8 +20,5 @@
 //    echo "Current language: " . $trans->getCurrentLanguage();
 //    echo "<br/>";
 //    echo "Danish prefix: " . $trans->getStaticText("ENGLISH");
-    
-    
->>>>>>> e8adb38997daeb86ee5bb92bb1f6c8e6fc818c69
 ?>
 

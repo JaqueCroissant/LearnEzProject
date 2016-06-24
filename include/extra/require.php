498a430ed4e7fc5d18a5fc87ebe957636448f559
mathiaskarl <mathiasklinke@gmail.com>
2016-06-24 12:47:40 +0200

    change password

--- include/extra/require.php
+++ include/extra/require.php
@@ -6,10 +6,12 @@
     require_once 'include/handler/sessionKeyHandler.php';
     require_once 'include/handler/rightsHandler.php';
     require_once 'include/handler/loginHandler.php';
+    require_once 'include/handler/userHandler.php';
     require_once 'include/handler/translationHandler.php';
     require_once 'include/handler/notificationHandler.php';
     require_once 'include/pages/pagelist.php';
 
     $loginHandler = new LoginHandler();
     $rightsHandler = new RightsHandler();
+    $userHandler = new UserHandler();
 ?>

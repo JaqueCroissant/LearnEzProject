6b21e0b148d337e50342988cba036f2ff0fbee22
mrhallberg <mr.hallberg@hotmail.com>
2016-06-24 10:55:30 +0200

    Rykket nogle require filer

--- ajax/ajax_require.php
+++ ajax/ajax_require.php
@@ -1,9 +1,12 @@
 <?php
+//Database handlers
 require_once '../include/extra/db.class.php';
 require_once '../include/handler/dbHandler.php';
+//Session handlers
 require_once '../include/handler/SessionKeyHandler.php';
-
-
-
-
-
+//Data handlers
+require_once '../include/handler/notificationHandler.php';
+//Datamodels extensions
+require_once '../include/class/orm.class.php';
+//Datamodels
+require_once '../include/class/notification.class.php';

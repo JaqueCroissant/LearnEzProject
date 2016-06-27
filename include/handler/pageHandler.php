98620a7bc4f0163465e6978df98aba36fcdfa4ed
mathiaskarl <mathiasklinke@gmail.com>
2016-06-27 13:14:15 +0200

    no message

--- include/handler/pageHandler.php
+++ include/handler/pageHandler.php
@@ -29,6 +29,10 @@ class pageHandler extends Handler {
             $user_type_id = $this->user_exists() ? $this->_user->user_type_id : 5;
             $pageData = DbHandler::getInstance()->ReturnQuery("SELECT * FROM page INNER JOIN translation_page ON translation_page.page_id = page.id INNER JOIN user_type_page ON user_type_page.page_id = page.id WHERE user_type_page.user_type_id = :user_type_id AND translation_page.language_id = :language_id", $user_type_id, TranslationHandler::getCurrentLanguage());
             
+            if(count($pageData) < 1) {
+                return;
+            }
+            
             $pageArray = array();
             foreach($pageData as $value) {
                 $pageArray[] = $value;

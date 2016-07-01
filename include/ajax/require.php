4762873cab897bbd4a8aff9149fb81f1671b0ec4
Krazh <moped47@gmail.com>
2016-07-01 10:43:14 +0200

    no message

--- include/ajax/require.php
+++ include/ajax/require.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once '../../include/extra/db.class.php';
 require_once '../../include/handler/handler.php';
 require_once '../../include/handler/dbHandler.php';
 require_once '../../include/handler/sessionKeyHandler.php';
@@ -7,8 +8,13 @@ require_once '../../include/handler/errorHandler.php';
 require_once '../../include/handler/translationHandler.php';
 
 
-require_once '../../include/extra/required_datamodels.php';
-
+require_once '../../include/class/orm.class.php';
+require_once '../../include/class/error.class.php';
+require_once '../../include/class/notification.class.php';
+require_once '../../include/class/page.class.php';
+require_once '../../include/class/rights.class.php';
+require_once '../../include/class/school.class.php';
+require_once '../../include/class/user.class.php';
 
 
 

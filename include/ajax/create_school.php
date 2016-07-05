6e87038bbbbc93e5dbd11b9952da14b91eebbbd0
Krazh <moped47@gmail.com>
2016-07-05 10:42:25 +0200

    Still doesn't work

--- include/ajax/create_school.php
+++ include/ajax/create_school.php
@@ -22,16 +22,16 @@ switch ($_POST['step']) {
         // case 2 is broken - datoen kan jeg ikke få konverteret korrekt til at min metode i schoolhandler kan håndtere den. 
         // Muligvis skal verify_subscription_end i schoolhandler refaktoreres.
         // 
-//        $d = date_parse_from_format($format, $_POST['school_subscription_end']);
-//        $d_string = $d['year'] . '/' . $d['month'] . '/' . $d['day'];
-//        if ($schoolHandler->create_school_step_two($schoolHandler->school, (int) $_POST['school_max_students'], $d_string)) {
-//            SessionKeyHandler::add_to_session("school_handler", $schoolHandler, true);
-//            $data_array["status_msg"] = "success";            
-//            $data_array['status_value'] = true;
-//        } else {
-//            $data_array['status_msg'] = $schoolHandler->error->title;
-//            $data_array["status_value"] = false;
-//        }
+        $d = date_parse_from_format($format, $_POST['school_subscription_end']);
+        $d_string = $d['year'] . '/' . $d['month'] . '/' . $d['day'];
+        if ($schoolHandler->create_school_step_two($schoolHandler->school, (int) $_POST['school_max_students'], $d_string)) {
+            SessionKeyHandler::add_to_session("school_handler", $schoolHandler, true);
+            $data_array["status_msg"] = "success";            
+            $data_array['status_value'] = true;
+        } else {
+            $data_array['status_msg'] = $schoolHandler->error->title;
+            $data_array["status_value"] = false;
+        }
 //        if (isset($_POST['school_subscription_end'])) {
 //    $data_array['date'] = date_parse_from_format($format, $_POST['school_subscription_end']);
 //}

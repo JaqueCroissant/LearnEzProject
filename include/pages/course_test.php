da57df89ea7b2336ecdfa08a0cede7d5ec9ccde5
mrhallberg <mr.hallberg@hotmail.com>
2016-07-29 09:55:39 +0200

    no message

--- include/pages/course_test.php
+++ include/pages/course_test.php
@@ -3,27 +3,29 @@ require_once 'require.php';
 require_once '../../include/handler/courseHandler.php';
 
 $handler = new courseHandler();
-$handler->load_test(6);
+
+
+if ($handler->load_test(6)) {
 ?>
 
 
-<div id="iframe_content" style="overflow:hidden;" max_slide="<?php echo $handler->test->total_steps; ?>" current_slide="<?php echo ((isset($handler->test->is_complete) && $handler->test->is_complete == 1) ? $handler->test->total_steps : (isset($handler->test->progress) ? $handler->test->progress : "1")); ?>">
-    <iframe scrolling="no" id="scaled-frame" class="course_iframe" src=<?php echo "../../LearnEZ/courses/" . $handler->test->path . "/index.php"; ?>></iframe>
-    <div id="course_bottombar" class="course_bar widget" style="color:<?php echo $handler->test->course_color ?>">
+<div id="iframe_content" style="overflow:hidden;" current_slide="<?php echo (($handler->test->is_complete == 1) ? $handler->test->total_steps : (isset($handler->test->progress) ? $handler->test->progress : "1")); ?>">
+    <div id="course_bar" class="course_bar widget" style="<?php echo "background-color: " . $handler->test->course_color . " !important;" ?>">
         <div class="course_slide_counter pull-left"></div>
         <div class="btn-group pull-right course_navigation">
             <a href="javascript:void(0)" value="go_backwards" class="course_action course_go_back btn btn-default" title=<?php echo TranslationHandler::get_static_text("PREVIOUS") ?>><i class="fa fa-chevron-left"></i></a>
             <a href="javascript:void(0)" value="go_forwards" class="course_action course_go_for btn btn-default" title=<?php echo TranslationHandler::get_static_text("NEXT") ?>><i class="fa fa-chevron-right"></i></a>
         </div>
     </div>
+    <iframe scrolling="no" id="scaled-frame" class="course_iframe" src="<?php echo "../../LearnEZ/courses/" . $handler->test->path . "/index.php"; ?>"></iframe>
 </div>
 
 <script>
     
 var can_be_clicked = false;
-var slide_reached = (int)($("#iframe_content").attr("current_slide"));
-var current_slide = (int)($("#iframe_content").attr("current_slide"));
-var max_slide = (int)($("#iframe_content").attr("max_slide"));
+var slide_reached = parseInt(($("#iframe_content").attr("current_slide")));
+var current_slide = parseInt(($("#iframe_content").attr("current_slide")));
+var max_slide = 0;
     
 function resize(){
     var ratio = $(".wrap").width() / 1024;
@@ -37,7 +39,6 @@ function resize(){
             "margin-top" : -(740 - 740 * ratio) / 2, 
             "margin-left" : -(1024 - 1024 * ratio) / 2
         });
-        $("#course_bottombar").css("margin-top", -(750 - 740 * ratio) / 2);
         $("#iframe_content").height(740 * ratio + 60);
         $(".course_bar").width(1024 * ratio);
     } 
@@ -51,13 +52,12 @@ function resize(){
             "margin-top" : 0, 
             "margin-left" : ($(".wrap").width() - 1024) / 2
         });
-        $("#course_bottombar").css("margin-top", -5);
         $("#iframe_content").height(800);
         $(".course_bar").width(1024);
     }
 }
 
-$(document).on("click", ".course_action", function(event){
+$(document).on("click", ".course_action", function(){
     if (can_be_clicked) {
         can_be_clicked = false;
         var window = document.getElementById("scaled-frame").contentWindow;
@@ -67,31 +67,27 @@ $(document).on("click", ".course_action", function(event){
             case "go_backwards" : if (current_slide !== 1) {window.cpAPIInterface.previous(); } break;
             default : break;
         }
-        setTimeout(function(){
-            can_be_clicked = true;
-        }, 500);
+        can_be_clicked = true;
     }
 });
 
-$(document).ready(function() {
-    resize();
+$(document).ready(function() {    
     $("#iframe_content").css("margin", "0 auto");
+    resize();
     $(window).on("resize", resize);
-    
-    var window = document.getElementById("scaled-frame").contentWindow;
-    window.addEventListener("moduleReadyEvent", function(){
+    var iframe_window = document.getElementById("scaled-frame").contentWindow;
+    iframe_window.addEventListener("moduleReadyEvent", function(){
         can_be_clicked = true;
-        current_slide = window.cpAPIInterface.getCurrentSlideIndex();
-        max_slide = window.cpAPIInterface.getVariableValue("rdinfoSlideCount");
+        max_slide = iframe_window.cpAPIInterface.getVariableValue("rdinfoSlideCount");
         $("#course_slide_counter").html("<p>" + current_slide + "/" +  max_slide + "</p>");
         if (current_slide === 1) {
             $(".course_go_back").attr("disabled", true);
         }
         if (current_slide === max_slide || current_slide === slide_reached) {
+            
             $(".course_go_for").attr("disabled", true);
         }
-        
-        window.cpAPIEventEmitter.addEventListener("CPAPI_SLIDEENTER", function(event){
+        iframe_window.cpAPIEventEmitter.addEventListener("CPAPI_SLIDEENTER", function(event){
             current_slide = event.Data.slideNumber;
             if (current_slide > slide_reached) {
                 slide_reached = current_slide;
@@ -110,7 +106,12 @@ $(document).ready(function() {
             }
             $(".course_slide_counter").html("<b>" + current_slide + "/" +  max_slide + "</b>");
         });
+        
+        iframe_window.cpAPIInterface.setVariableValue("cpCmndGotoSlide", current_slide - 1);
     });
 });
 </script>
+<?php
+}
+?>
 

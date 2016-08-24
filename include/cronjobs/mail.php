44daf06f07b15e8ddb55a308f3d2c7984dca8831
mathiaskarl <mathiasklinke@gmail.com>
2016-08-24 14:37:53 +0200

    no message

---
+++ include/cronjobs/mail.php
@@ -0,0 +1,6 @@
+<?php //
+//require_once '../handler/dbHandler.php';
+//DbHandler::get_instance()->query("DELETE FROM user_mail WHERE sender_folder_id = '7' AND receiver_folder_id = '7'");
+//
+//"DELETE m FROM user_mail m LEFT JOIN users u ON u.id = m.receiver_folder_id WHERE f.id IS NULL"
+//DbHandler::get_instance()->query("DELETE FROM mail")

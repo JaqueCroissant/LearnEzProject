9156634068d5d301c889d9a5c9197133c5db9bcc
mrhallberg <mr.hallberg@hotmail.com>
2016-08-26 16:15:42 +0200

    no message

--- courses/core/copy/index.php
+++ courses/core/copy/index.php
@@ -1,8 +1,8 @@
-
-
-<!DOCTYPE html>
 <?php
     session_start();
+?>
+<!DOCTYPE html>
+<?php
     require_once '../../../include/extra/db.class.php';
     require_once '../../../include/class/orm.class.php';
     require_once '../../../include/class/user.class.php';
@@ -26,7 +26,7 @@
     $name = explode("/", $_SERVER['REQUEST_URI'])[4];
     
     if(!$loginHandler->check_login() || !$courseHandler->check_test_name($name)) {
-        die();
+        die("Direct access not permitted.");
     }
 ?>
 <html lang="en">
@@ -40,56 +40,53 @@
 <style>#initialLoading{background:url(assets/htmlimages/loader.gif) no-repeat center center;background-color:#ffffff;position:absolute;margin:auto;top:0;left:0;right:0;bottom:0;z-index:10010;}</style>
 
 <script>
-if (window.self === window.top) {
-    window.stop();
-}
-var deviceReady = false;
-var initCalled = false ;
-var initialized = false;
+if (window.self !== window.top) {
+    var deviceReady = false;
+    var initCalled = false ;
+    var initialized = false;
 
-function onBodyLoad()
-{
-    if(typeof window.device === 'undefined')
+    function onBodyLoad()
     {
-        document.addEventListener("deviceready", onDeviceReady, false);
-    }
-    else
-    {
-        onDeviceReady();
-    }
-}
-
-function onDeviceReady()
-{
-    deviceReady = true ;
-    if(initCalled === true) {initializeCP();}
-}
-
-function initializeCP()
-{
-    if(initialized) {return;}
-    initCalled = true;
-    if(cp.pg && deviceReady === false) {return;}
-
-    function cpInit()
-    {
-        document.getElementById("course").innerHTML = " <div class='cpMainContainer' id='cpDocument' style='left: 0px; top:0px;' >	<div id='main_container' style='top:0px;position:absolute;'>	<div id='projectBorder' style='top:0px;left:0px;position:absolute;display:block'></div>	<div class='shadow' id='project_container' style='left: 0px; top:0px;position:absolute;' >	<div id='project' class='cp-movie' style='width:1024px ;height:740px '>		<div id='project_main' class='cp-timeline cp-main'>			<div id='div_Slide' onclick='cp.handleClick(event)' style='top:0px; width:1024px ;height:740px ;position:absolute;-webkit-tap-highlight-color: rgba(0,0,0,0);'></div>			<canvas id='slide_transition_canvas'></canvas>		</div>		<div id='autoplayDiv' style='display:block;text-align:center;position:absolute;left:0px;top:0px;'>			<img id='autoplayImage' src='' style='position:absolute;display:block;vertical-align:middle;'/>			<div id='playImage' tabindex='9999' role='button' aria-label='play' onkeydown='cp.CPPlayButtonHandle(event)' onClick='cp.movie.play()' style='position:absolute;display:block;vertical-align:middle;'></div>		</div>	</div>	<div id='toc' style='left:0px; float:left;position:absolute;-webkit-tap-highlight-color: rgba(0,0,0,0);'>	</div>	<div id='playbar' style='left:0px; float:left;position:absolute'>	</div>	<div id='cc' style='left:0px; float:left;position:absolute;visibility:hidden;pointer-events:none;' onclick='cp.handleCCClick(event)'>		<div id='ccText' style='left:0px;float:left;position:absolute;width:100%;height:100%;'>		<p style='margin-left:8px;margin-right:8px;margin-top:2px;'>		</p>		</div>		<div id='ccClose' style='background-image:url(./assets/htmlimages/ccClose.png);right:0px; float:right;position:absolute;cursor:pointer;width:13px;height:11px;' onclick='cp.showHideCC()'>		</div>	</div>	<div id='gestureIcon' class='gestureIcon'>	</div>	<div id='gestureHint' class='gestureHintDiv'>		<div id='gImage' class='gesturesHint'></div>	</div>	<div id='pwdv' style='display:block;text-align:center;position:absolute;width:100%;height:100%;left:0px;top:0px'></div>	<div id='exdv' style='display:block;text-align:center;position:absolute;width:100%;height:100%;left:0px;top:0px'></div>	</div>	</div></div><div id='blockUserInteraction' class='blocker' style='width:100%;height:100%;'>	<table style='width:100%;height:100%;text-align:center;vertical-align:middle' id='loading' class='loadingBackground'>		<tr style='width:100%;height:100%;text-align:center;vertical-align:middle'>			<td style='width:100%;height:100%;text-align:center;vertical-align:middle'>				<image id='preloaderImage'></image>				<div id='loadingString' class='loadingString'>Loading...</div>			</td>		</tr>	</table></div> <div id='initialLoading'></div>";
-        cp.DoCPInit();
-        var lCpExit = window["DoCPExit"];
-        window["DoCPExit"] = function()
+        if(typeof window.device === 'undefined')
         {
-            if(cp.UnloadActivties)
-                cp.UnloadActivties();
-            lCpExit();
-        };
+            document.addEventListener("deviceready", onDeviceReady, false);
+        }
+        else
+        {
+            onDeviceReady();
+        }
     }
 
-    cpInit();
-    initialized = true;
-}
-
-(function()
+    function onDeviceReady()
     {
+        deviceReady = true ;
+        if(initCalled === true) {initializeCP();}
+    }
+
+    function initializeCP()
+    {
+        if(initialized) {return;}
+        initCalled = true;
+        if(cp.pg && deviceReady === false) {return;}
+
+        function cpInit()
+        {
+            document.getElementById("course").innerHTML = " <div class='cpMainContainer' id='cpDocument' style='left: 0px; top:0px;' >	<div id='main_container' style='top:0px;position:absolute;'>	<div id='projectBorder' style='top:0px;left:0px;position:absolute;display:block'></div>	<div class='shadow' id='project_container' style='left: 0px; top:0px;position:absolute;' >	<div id='project' class='cp-movie' style='width:1024px ;height:740px '>		<div id='project_main' class='cp-timeline cp-main'>			<div id='div_Slide' onclick='cp.handleClick(event)' style='top:0px; width:1024px ;height:740px ;position:absolute;-webkit-tap-highlight-color: rgba(0,0,0,0);'></div>			<canvas id='slide_transition_canvas'></canvas>		</div>		<div id='autoplayDiv' style='display:block;text-align:center;position:absolute;left:0px;top:0px;'>			<img id='autoplayImage' src='' style='position:absolute;display:block;vertical-align:middle;'/>			<div id='playImage' tabindex='9999' role='button' aria-label='play' onkeydown='cp.CPPlayButtonHandle(event)' onClick='cp.movie.play()' style='position:absolute;display:block;vertical-align:middle;'></div>		</div>	</div>	<div id='toc' style='left:0px; float:left;position:absolute;-webkit-tap-highlight-color: rgba(0,0,0,0);'>	</div>	<div id='playbar' style='left:0px; float:left;position:absolute'>	</div>	<div id='cc' style='left:0px; float:left;position:absolute;visibility:hidden;pointer-events:none;' onclick='cp.handleCCClick(event)'>		<div id='ccText' style='left:0px;float:left;position:absolute;width:100%;height:100%;'>		<p style='margin-left:8px;margin-right:8px;margin-top:2px;'>		</p>		</div>		<div id='ccClose' style='background-image:url(./assets/htmlimages/ccClose.png);right:0px; float:right;position:absolute;cursor:pointer;width:13px;height:11px;' onclick='cp.showHideCC()'>		</div>	</div>	<div id='gestureIcon' class='gestureIcon'>	</div>	<div id='gestureHint' class='gestureHintDiv'>		<div id='gImage' class='gesturesHint'></div>	</div>	<div id='pwdv' style='display:block;text-align:center;position:absolute;width:100%;height:100%;left:0px;top:0px'></div>	<div id='exdv' style='display:block;text-align:center;position:absolute;width:100%;height:100%;left:0px;top:0px'></div>	</div>	</div></div><div id='blockUserInteraction' class='blocker' style='width:100%;height:100%;'>	<table style='width:100%;height:100%;text-align:center;vertical-align:middle' id='loading' class='loadingBackground'>		<tr style='width:100%;height:100%;text-align:center;vertical-align:middle'>			<td style='width:100%;height:100%;text-align:center;vertical-align:middle'>				<image id='preloaderImage'></image>				<div id='loadingString' class='loadingString'>Loading...</div>			</td>		</tr>	</table></div> <div id='initialLoading'></div>";
+            cp.DoCPInit();
+            var lCpExit = window["DoCPExit"];
+            window["DoCPExit"] = function()
+            {
+                if(cp.UnloadActivties)
+                    cp.UnloadActivties();
+                lCpExit();
+            };
+        }
+
+        cpInit();
+        initialized = true;
+    }
+
+    (function() {
         if(document.documentMode < 9)
         {
             document.getElementById("course").innerHTML = "";
@@ -127,10 +124,16 @@ function initializeCP()
                     });
                 }
                 document.getElementsByTagName('head')[0].appendChild(script);
-                
+
             },1);
         },false);
     })();
+}
+else {
+    window.onload = function(){
+        document.write("Direct access not permitted.");
+    };  
+}
 
 </script>
 
